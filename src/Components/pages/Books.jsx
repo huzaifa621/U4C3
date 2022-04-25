f2196e6305b7b5fb208062c7dbd732892ef771f9
huzaifa621 <99542545+huzaifa621@users.noreply.github.com>
2022-04-25 16:25:27 +0530

    Initial commit

---
+++ src/Components/pages/Books.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BookCard } from "./BookCard";
+import styled, { css } from "styled-components";
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+  /* add required style here */
+`;
+
+const Books = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // make a GET request to http://localhost:8080/books to get all the books data
+    fetch("http://localhost:8080/books").then(function (res) {
+      res.json().then(function (data) {
+        setData(data);
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Books</h1>
+      <Grid data-testid="books-container">
+        {/* {!!data && 
+          // map thorugh the data and use <BookCard/> component to display each book
+          } */}
+        {data.map((el) => {
+          return <BookCard key={el.id} props={el} />;
+        })}
+      </Grid>
+    </>
+  );
+};
+export default Books;
